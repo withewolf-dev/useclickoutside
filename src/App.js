64bf4e486501045183fbd8ddad8f29b534db7705
withewolf-dev <itskashyap26@gmail.com>
2022-02-08 22:55:05 +0530

    second push

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./App.css";
 
 const items = ["cars", "bikes", "cycles", "plane"];
+
+//useClickOutside(ref,callback,visible)
 function App() {
   const [selectedIndex, setselectedIndex] = useState(0);
   const [visibility, setvisibility] = useState(false);
+
+  const dropdownref = useRef();
+
+  const showDropDown = () => {
+    setvisibility(!visibility);
+  };
+
+  const hideDropDown = () => {
+    setvisibility(false);
+  };
+  const select = (index) => {
+    hideDropDown();
+    setselectedIndex(index);
+  };
   return (
     <div className="flex justify-center">
       <div>
         <button
           className="bg-slate-300 px-4 h-8 w-16 border-2 border-blue-400 rounded-md"
-          onClick={() => {
-            setvisibility(!visibility);
-          }}
+          onClick={showDropDown}
         >
           {items[selectedIndex]}
         </button>
@@ -21,16 +35,11 @@ function App() {
           <>
             {index !== selectedIndex && (
               <ul
+                ref={dropdownref}
                 className={`  ${visibility ? "invisible" : "visible"}`}
                 key={index}
               >
-                <li
-                  className="cursor-pointer "
-                  onClick={() => {
-                    setselectedIndex(index);
-                    setvisibility(false);
-                  }}
-                >
+                <li className="cursor-pointer " onClick={select(index)}>
                   {item}
                 </li>
               </ul>
